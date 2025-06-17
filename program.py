a195eed1a1fba6216c920d10e316bb7ecb04838c
Steven Samuel <samuelsteven6@gmail.com>
2025-06-17 22:41:32 +0700

    Enhance data visualization by plotting daylight duration over time for Cluster 1

--- program.py
+++ program.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Read train.csv
 train_df = pd.read_csv('datasets/train.csv')
@@ -8,7 +10,25 @@ cluster_2 = train_df[train_df['cluster_id'] == 'cluster_2']
 cluster_3 = train_df[train_df['cluster_id'] == 'cluster_3']
 cluster_4 = train_df[train_df['cluster_id'] == 'cluster_4']
 
+cluster_1['date'] = pd.to_datetime(cluster_1['date'])
+plt.figure(figsize=(10, 5))
+#plt.plot(cluster_1['date'], cluster_1['temperature_2m_max'],color='r')
+#plt.plot(cluster_1['date'], cluster_1['temperature_2m_min'],color='g')
+#plt.xlabel('Date')
+#plt.ylabel('Temperature in Celsius')
+#plt.title('Temperature Maximum and Minimum Over Time for Cluster 1')
+#plt.xticks(rotation=45)
+#plt.tight_layout()
+#plt.show()
 
+cluster_1['daylight_duration'] = cluster_1['daylight_duration'] / 3600
+plt.plot(cluster_1['date'], cluster_1['daylight_duration'], color = 'b')
+plt.xlabel('Date')
+plt.ylabel('Daylight Duration')
+plt.title('Daylight Duration Over Time for Cluster 1')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
 # All columns inside the train.csv:
 #        'ID', 'date', 'cluster_id', 'electricity_consumption',
 #       'temperature_2m_max', 'temperature_2m_min', 'apparent_temperature_max',
